305931ee4247718846cc03d82e465eab89f83508
Bart Wijnants <bart.w@sparkcentral.com>
2019-11-30 10:04:25 +0100

    Bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import "./index.scss";
 
 export default () => (
   <>
@@ -7,6 +8,13 @@ export default () => (
       <title>HDP</title>
       <meta name="description" content="Haskey Development Program" />
     </Helmet>
-    <div>Hello world!</div>
+    <section className="hero is-primary is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">HDP</h1>
+          <h2 className="subtitle">Haskey Development Program</h2>
+        </div>
+      </div>
+    </section>
   </>
 );
