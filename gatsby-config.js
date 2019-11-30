a3d8411b07d6b8290869443356ea81264cfca5d2
Bart Wijnants <bart.w@sparkcentral.com>
2019-11-30 09:40:58 +0100

    head + manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Haskey Development Program`,
+        short_name: `HDP`,
+        start_url: `/`,
+        background_color: `#266dd3`,
+        theme_color: `#353535`,
+        display: `standalone`,
+        icon: `src/images/haskey.svg`
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <>
+    <Helmet>
+      <title>HDP</title>
+      <meta name="description" content="Haskey Development Program" />
+    </Helmet>
+    <div>Hello world!</div>
+  </>
+);
